e16a3888b63d79f0ee39d7a436a946e49c80c7f5
Rosa <ngr@newschool.edu>
2019-08-15 10:02:27 -0400

    changed idea, updated all pages with new pages

---
+++ script.js
@@ -0,0 +1,31 @@
+// const tip = document.getElementById('tooltip');
+// // make visible
+// tip.addEventListener("mouseover", function(){
+// 	document.getElementById("tooltiptext").style.visibility = "visible";
+// });
+
+// // make invisible
+// tip.addEventListener("mouseout", function (){
+// 	document.getElementById("tooltiptext").style.visibility = "hidden";
+// });
+
+
+// const tip1 = document.getElementById('tooltip1');
+// // make visible
+// tip1.addEventListener("mouseover", function(){
+// 	document.getElementById("tooltiptext1").style.visibility = "visible";
+// });
+
+// // make invisible
+// tip1.addEventListener("mouseout", function (){
+// 	document.getElementById("tooltiptext1").style.visibility = "hidden";
+// });
+
+
+
+
+// <script>
+// function myFunction() {
+//   document.getElementById("demo").innerHTML= "hello";}
+  
+// </script>
